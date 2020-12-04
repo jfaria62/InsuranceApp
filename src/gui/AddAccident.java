b64d4285abdf9dde3cd442b970bed531e7ef24ea
jfaria62 <jfaria62@gmail.com>
2020-12-04 10:06:28 -0500

    Minor edits. Exit button, additional text, element screen positions

--- src/gui/AddAccident.java
+++ src/gui/AddAccident.java
@@ -69,8 +69,10 @@ public class AddAccident extends Application{
         Button submitBtn = new Button("Submit");
         Button addVehicleBtn = new Button("Add Vehicle Involved");
         Button submitNewDriverBtn = new Button("Submit Driver");
-        Button cancelBtn = new Button("Cancel");        
-                
+        Button cancelBtn = new Button("Cancel"); 
+        //prevent users from entering bad date information
+        datePicker.getEditor().setDisable(true); 
+        
         //add all elements to current scene
   		Text scenetitle = new Text("Create An Accident Report");
   		scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
